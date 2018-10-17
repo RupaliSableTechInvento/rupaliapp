d57c371a4ee73958366887136e9b0d6ffb40936a
RupaliSableTechInvento <37444188+RupaliSableTechInvento@users.noreply.github.com>
2018-10-17 11:59:23 +0530

    Port added

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
-
+const PORT= process.env.PORT || 3000;
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Frist App!');
-}).listen(8080);
+}).listen(PORT);
